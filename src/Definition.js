192d79cec8792452612ac7e84bc2661030274c58
20b01a12i0 <20b01a12i0@svecw.edu.in>
2022-05-16 15:24:31 +0530

    secondproject

---
+++ src/Definition.js
@@ -0,0 +1,29 @@
+import logo from './logo123.png';
+function Definition(){
+    return(
+        <div className="main-class">
+        <div className="sub-class">
+            <img src={logo}  />
+        </div>
+        <div className="head">
+        <div className="title">
+            <h3>Lizard</h3>
+        </div>
+        <div className="body">
+            <p>Lizards are a widespeard group of squamate reptiles,with over 6,000 species ,ranging across all continents except antarctica</p>
+        </div>
+        <div className="btn">
+        <button>
+            <a>SHARE</a>
+        </button>
+        <button>
+            <a>LEARN MORE</a>
+        </button>
+        </div>
+        </div>
+        
+   
+    </div>
+        )
+}
+export default Definition;
